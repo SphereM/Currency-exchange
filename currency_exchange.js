7612de8efd46c458ad7381f2dc72031240cac351
Max Kuybus <icenova360@outlook.com>
2019-12-04 11:01:41 -0700

    convert canadian

--- currency_exchange.js
+++ currency_exchange.js
@@ -1,8 +1,35 @@
 function ready () {
 
+    let form = document.forms.country_select;
 
+    let country_ = form.elements.country;
 
+    let _country = country_.value;
 
+    let input = form.elements.amount;
+
+    
+    amount.oninput = function() {
+        console.log(country.value);
+        if (country.value == "canada") {
+            
+            let amount_ = input.value;
+            rounding = amount_*1.31951;
+            rounded = Math.round(rounding*100)/100;
+            output.innerHTML = rounded;
+            loss.innerHTML = "-$"+amount_*0.05;
+        }
+        if (country.value == "europe") {
+            
+            let amount_ = input.value;
+            rounding = amount_*2;
+            rounded = Math.round(rounding*100)/100;
+            output.innerHTML = rounded;
+            loss.innerHTML = "-$"+amount_*0.05;
+        }
+
+        
+    }
 
     
 }
